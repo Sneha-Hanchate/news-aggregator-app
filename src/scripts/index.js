b8b8e0b5fe50e31ee1c142b9cf674beb0b9dbd08
Sneha-Hanchate <64279394+Sneha-Hanchate@users.noreply.github.com>
2020-07-21 15:24:15 +0530

    0

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,91 +1,51 @@
-//const url ='http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-19&sortBy=publishedAt&apiKey=11d8247de9d6481491bc5c82ccd6624c';
-
-
-
-// $(document).ready(async function news(){
-// 	let response = await fetch(url);
-// 	let data = await response.json();
-// 	//debugger;
-//   let news = ''
-//   console.log(data);
-// 	data.articles.forEach(article=> {
-// 		// put your loop code over here 
-// 				news+= `
-//                     <li class="article">
-//                        <img class="article-img" src="${article.urlToImage}"/>
-
-//                        <h2 class="article-title">${article.title}</h2>
-
-//                        <p class="article-description">${article.description}</p>
-
-//                        <span class="article-author">${article.author}</span>
-
-//                        <a class="article-link" href="${article.url}" target="_blank"></a>
-//                     </li>
-//                 `;
-// 			});//iteration
-//          /* let response = await fetch(url);
-// 		  let data = await response.json();
-//         //x.then(res => res.json())
-// 		data.forEach((x) => {});
-//         .then((articles) => {
-//             let output="";
-//             news.articles.forEach(article=>{
-                
-// 					   console.log("hii ".output);
-//             });*/
-			
-//             document.getElementById("news-articles").innerHTML=news;
-//         })
-       
-    
-
-$(document).ready(function(){ 
-  console.log("loadin...");
-    $('#search').keypress(()=>{
-      console.log("keypress...");
-      let searchField = $("#search").val();
-      const url1 = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-19&sortBy=publishedAt&apiKey=11d8247de9d6481491bc5c82ccd6624c`;
-      //  const url1 ='https://api.linein.tech/common/v2/index.php/API/loadNews/bitcoin';
-      if(searchField !== ""){
-        $.ajax({
-          url: url1,
-          // crossDomain: true,
-          method: "GET",
-          dataType: "json",
-          
-            success: function(news){
-              console.log("news fetched :: "+news);
-            let output1 = "";
-            let articles = news.articles;
-            
-            for(var art in articles){
-              output1 +=`
-                <li class="article">
-                   <img class="article-img" src="${art.urlToImage}"/>
-                   <h2 class="article-title">${art.title}</h2>
-                   <p class="article-description">${art.description}</p>
-                   <span class="article-author">${art.author}</span>
-                   <a class="article-link" href="${art.url}" target="_blank"></a>
-                </li>`;
-            }
-			// console.log(output2);
-      //       if(output2 !== ""){
-      //         document.getElementById("news-articles").innerHTML=output2;  
-      //       }
-      //       else{
-			// 	news_articles.innerHTML='<li class="not-found">No article was found based on the search.</li>';
-
-			// 	        } 
-          } ,
-          fail: function(xhr, textStatus, errorThrown){
-            alert('request failed');
-         } 
-        }); 
-      }
-	 
+let input = document.querySelector("input");
+let newsarticles = document.getElementById("news-articles");
+//function final is here... contains DOM
+function final(Result) {
+  let found = "";
+  if (Result.length == 0) {
+    Found = `<h2>No article was found based on the search.</h2>`;
+    newsarticles.innerHTML = Found;
+    console.log("else");
+  } else {
+    Result.forEach((article) => {
+      let urlToImage = article.urlToImage;
+      let title = article.title;
+      let description = article.description;
+      let author = article.author;
+      let url1 = article.url;
+      found += `
+      <li class="article">
+    <img src="${urlToImage}" class="article-img" alt="image not found">
+    <div>
+      <h2 class="article-title">${title}</h2>
+      <p class="article-description">${description}</p>
+      <p><span class="article-author"><a class="article-link" href="${url1}">${author}</a></span></p>
+    </div>
+  </li>`;
+      newsarticles.innerHTML = found;
     });
+  }
+}
+//function swapnil is here ... will fetch news
+async function swapnil(url) {
+  let response = await fetch(url);
+  let result = await response.json();
+  let finalResult = result.articles;
+  final(finalResult);
+}
+// when page loads, this lines will execute
+let url = `http://newsapi.org/v2/top-headlines?country=in&apiKey=728d17fec990486eba3657920589bfe8`;
+swapnil(url);
+// when we type in search bar and press enter below lines will execute
+input.addEventListener("keydown", (f) => {
+  if (f.keyCode === 13) {
+    console.log(f.target.value);
+    let topic = f.target.value;
+    let url = `http://newsapi.org/v2/everything?q=${topic}&apiKey=728d17fec990486eba3657920589bfe8`;
+    swapnil(url);
+  }
 });
 
-
-
+//my new api    728d17fec990486eba3657920589bfe8
+//my api// http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-19&sortBy=publishedAt&apiKey=11d8247de9d6481491bc5c82ccd6624c
